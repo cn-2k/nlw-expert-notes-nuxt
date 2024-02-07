c6f4111f761c785249e957b81c3662ee44f03c1b
Caio Vinicius <caio.vinnicius2k@gmail.com>
2024-02-07 13:05:58 -0300

    Aula 1

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mx-auto my-12 max-w-6xl space-y-6">
+    <img src="~/assets/logo-nlw-expert.svg" alt="NLW Expert" />
+    <form class="w-full">
+      <input
+        type="text"
+        placeholder="Busque em suas notas..."
+        class="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+      />
+    </form>
+
+    <div class="h-px bg-slate-700"></div>
+
+    <div class="grid auto-rows-[250px] grid-cols-3 gap-6">
+      <NewNoteCard />
+      <NoteCard />
+      <NoteCard />
+      <NoteCard />
+    </div>
+  </div>
+</template>
---
+++ components/new-note-card.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="space-y-3 rounded-md bg-slate-700 p-5">
+    <span class="text-sm font-medium text-slate-200">Adicionar nota</span>
+    <p class="text-sm leading-6 text-slate-400">
+      Grave uma nota em áudio que será convertida para texto automaticamente.
+    </p>
+  </div>
+</template>
